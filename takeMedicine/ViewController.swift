cd8f070c417b9203e634bf89821b3ada6b6a099f
kdh5018 <rlaehgns745@gmail.com>
2023-02-08 15:02:57 +0900

    Initial Commit

---
+++ takeMedicine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  takeMedicine
+//
+//  Created by 김도훈 on 2023/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
